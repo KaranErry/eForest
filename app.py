0deb2cae09d7a2aed1878e34000f07c931ad76b8
Jhonatan Cruz <jcruz3@drew.edu>
2017-10-25 17:14:40 -0400

    added a path for all the forms

--- app.py
+++ app.py
@@ -6,9 +6,27 @@ app=Flask(__name__)
 def home():
     return render_template('home.html')
 
+#student form
 @app.route('/studentForm')
 def studentForm():
     return render_template('studentForm.html')
+
+#Professor Form
+@app.route('/professorForm')
+def professorForm():
+    return render_template('professorForm.html')
+
+#Department Head/Chair Form
+@app.route('/departmentHeadForm')
+def departmentHeadForm():
+    return render_template('departmentHeadForm.html')
+
+#Registrar/Dean Form
+@app.route('/deanForm')
+def deanForm():
+    return render_template('deanForm.html')
+
+
 
 @app.route('/login')
 def login():
