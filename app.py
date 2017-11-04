e37518e1305da12a8f4a9a17479547130c41969b
Karan Erry <kerry@drew.edu>
2017-11-04 01:36:43 -0400

    Cleaning up the file.

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import google_auth_oauthlib.flow
 import google.oauth2.credentials
 import oauth2client
 from googleapiclient.discovery import build
-import jsonpickle
-import json
 import requests
 
 from flask import Flask, render_template, session, redirect, request, url_for
@@ -42,95 +40,63 @@ def deanForm():
 # Login landing page
 @app.route('/login')
 def login():
-    global loginJustBegun
-    loginJustBegun = True
+    global loginJustBegun   # FOR TESTING
+    loginJustBegun = True   # FOR TESTING
     return render_template('login.html')
 
 # Process OAuth authorization
 @app.route('/login/processLogin')
 def processLogin():
-    if loginJustBegun:
-        session.clear()
+    if loginJustBegun:      # FOR TESTING TODO: When finalising, remove all related "FOR TESTING" statements that clear the session whenever the login page is (re)loaded.
+        session.clear()     # FOR TESTING
     if 'credentials' not in session:
         print("IIIIIIIIIIIIIII creds not in session")
         return redirect(url_for('authorize'))
-    # print (session)
     print("IXIXIXIXIXIXIXI creds in session")
 
     # Load credentials from the session:
     credentials = google.oauth2.credentials.Credentials(**session['credentials'])
 
-    # Load the Open ID Discovery Document from Google's URL and then unpack it into a JSON dict:
-    # discoveryDoc = json.loads(requests.get('https://accounts.google.com/.well-known/openid-configuration').text)
-
-    # Make HTTPS GET request to the user-info endpoint
-    # print(jsonpickle.encode(credentials))
-    # payload = {'token': credentials.token}
-    # print(payload)
-
-    # print(json.loads(jsonpickle.encode(credentials))['token']['token'])
-    # response = json.loads(requests.get(discoveryDoc['userinfo_endpoint'], params=payload).text)
-
-    # response = requests.get('https://www.googleapis.com/plus/v1/people/me/openIdConnect', params=payload)
-    # print("URURURURURURURURURURU")
-    # print(json.loads(response.text))
-
-    # print(",\n".join(", ".split(json.loads(response.text))))
-
-    # Load credentials from the session:
-    # credentials = google.oauth2.credentials.Credentials(jsonpickle.decode(session['credentials']))
+    # Build the service object for the Google OAuth v2 API:
     oauth = build('oauth2', 'v2', credentials=credentials)
-    print (type(oauth))
+
+    # Call methods on the service object to return a response with the user's info:
     userinfo = oauth.userinfo().get().execute()
-    print(type(userinfo))
-    print (userinfo)
-    # print("YTYTYTYTYTYTYTY creds", type(credentials))
-    # print(jsonpickle.encode(credentials))
-    # print("TYTYTYTYTYTYTYT userinfo", type(userInfo))
-    # print(userInfo)
-    return "Hello, " + userinfo['name'] + "!"
-    # return ("Hello, ", json.load(userinfo).get('name'), '!')
+
     # TODO: (Eventually) Since the hd parameter in the authorization can be modified by the user, check that the user signed in with a drew.edu email and if not, log them out and direct to the login landing page again.
     # TODO: Obtain user's profile info
     # TODO: Store user's profile info in persistent storage.
+
+    # This section of commented-out code is specifically for converting the login flow to OIDC-compliant in the future
+        # Load the Open ID Discovery Document from Google's URL and then unpack it into a JSON dict:
+        # discoveryDoc = json.loads(requests.get('https://accounts.google.com/.well-known/openid-configuration').text)
+
+    return "Hello, " + userinfo['name'] + "!"
 
 # Authorize using OAuth
 @app.route('/login/authorize')
 def authorize():
     # Construct the Flow object:
-    # global flow # TODO: Remove the global and ask StackOverflow why the flow.fetch_token() call in processAuthCallback() throws a "global value flow is not defined" error. Global values are apparently not great programming practice in python.
-    session.clear()
-    # global flow
     flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
     'client_secret_217930784500-l9noq9hdupkormpjoamplnvsp3078q88.apps.googleusercontent.com.json',
-    scopes = [
-    'profile',
-    'email',
-    # 'openid'
-    ]
-    )
-    print("XXXXXXXXXXXX", type(flow))
-    # flow = oauth2client.flow_from_clientsecrets(
-    # 'client_secret_217930784500-l9noq9hdupkormpjoamplnvsp3078q88.apps.googleusercontent.com.json',
-    # scope = ['profile', 'email', 'openid']
-    # )
+    scopes = ['profile', 'email'])
+
     # Set the Redirect URI:
     flow.redirect_uri = url_for('processAuthCallback', _external = True)
-    print("XXXXXXXXXXXX", type(flow))
+
     # Generate URL for request to Google's OAuth 2.0 server:
     authorization_url, state = flow.authorization_url(
         # Enable offline access so as to be able to refresh an access token withou re-prompting the user for permission
         access_type = 'offline',
         # Enable incremental authorization
         include_granted_scopes = 'true',
-        #
+        # Specify the Google Apps domain so that the user can only login using a 'drew.edu' email address.
+        # NOTE: This can be overridden by the user by modifying the request URL in the browser, which is why the processLogin() view  double-checks the domain of the logged-in user's email to ensure it's a 'drew.edu' email address.
         hd = 'drew.edu'
         )
+
     # Store the state so the callback can verify the auth server response:
     session['state'] = state
-    # session['flow'] = jsonpickle.encode(flow)
-    # print("XXXXXXXXXAAA", type(jsonpickle.decode(session.get('flow'))))
-    # print("XXXXXXXXXHHH", type(jsonpickle.decode(session['flow'])))
 
     return redirect(authorization_url)
 
@@ -139,30 +105,23 @@ def authorize():
 def processAuthCallback():
     # Specify the state when creating the flow in the callback so that it can verified in the authorization server response:
     state = session['state']
-    # flow = jsonpickle.decode(session['flow'])
-    # print("XXXXXXXXXIII", type(flow))
 
+    # Reconstruct the flow object:
     flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
     'client_secret_217930784500-l9noq9hdupkormpjoamplnvsp3078q88.apps.googleusercontent.com.json',
-    scopes = [
-    'profile',
-    'email',
-    # 'openid'
-    ]
-    )
+    scopes = ['profile', 'email'])
     flow.redirect_uri = url_for('processAuthCallback', _external = True)
 
     # Use the authorization server's response to fetch the OAuth 2.0 tokens:
     authorization_response = request.url.strip()
     flow.fetch_token(authorization_response = authorization_response)
+
     # Store credentials in the session:
     # TODO: When migrating to production, store these credentials in a persistent database instead.
-    credentials = flow.credentials
-    # session['credentials'] = credentials_to_dict(credentials)
-    session['credentials'] = credentials_to_dict(credentials)
+    session['credentials'] = credentials_to_dict(flow.credentials)
 
-    global loginJustBegun
-    loginJustBegun = False
+    global loginJustBegun   # FOR TESTING
+    loginJustBegun = False  # FOR TESTING
     return redirect(url_for('processLogin'))
 
 def credentials_to_dict(credentials):
@@ -172,9 +131,6 @@ def credentials_to_dict(credentials):
           'client_id': credentials.client_id,
           'client_secret': credentials.client_secret,
           'scopes': credentials.scopes}
-
-
-
 
 if __name__== "__main__":
     app.run(debug=True)
